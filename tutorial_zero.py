21e2830fdf7c56e6869ca20e6d8f658bfeb7ad23
Romain DESSART <romaindessart@gmail.com>
2020-09-30 10:26:35 +0200

    tuto_zero : implementing OOP

--- tutorial_zero.py
+++ tutorial_zero.py
@@ -7,15 +7,38 @@ import pygame
 from pygame.locals import *
 
 
-def main() -> int:
-    pygame.init()
-    window = pygame.display.set_mode((640, 480))
-    while True:
+class Window():
+    """Handle a graphical windows"""
+    def __init__(self, resolution: tuple):
+        """Default constructor"""
+        self.window = pygame.display.set_mode(resolution)
+        self.resolution = resolution
+        self.run = True
+
+    def _handle_event(self):
+        """handle event in our class"""
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
-            break
+            self.run = False
+
+    def update(self):
+        """Update"""
+
+    def draw(self):
+        """Draw call"""
         pygame.display.update()
-    pygame.quit()
+
+    def game_loop(self):
+        while self.run:
+            self._handle_event()
+            self.update()
+            self.draw()
+
+
+def main():
+    pygame.init()
+    Win = Window((640, 480))
+    Win.game_loop()
 
 
 if __name__ == "__main__":
